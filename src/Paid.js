c81cc4c33cd74bc2d0e923341761b16e21667863
Louis Vella <lou.vella1@gmail.com>
2024-09-27 13:35:25 -0400

    Final Commit!

--- src/Paid.js
+++ src/Paid.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Paid = () => {
   return (
-    <div>
+    <div className="homepage">
       <h2>Paid</h2>
       <p>Thank you for your credit card payment!</p>
+      <p>To continue shopping, please click on the Home button, or simply select the Logout button to complete your session!</p>
     </div>
   );
 };
